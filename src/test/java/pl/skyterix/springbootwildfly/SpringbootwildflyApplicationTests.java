cd3e34d87a4211877ea1e21050c906310e67df57
Skyterix <skyterix1991@gmail.com>
2019-12-02 20:27:34 +0100

    Initial commit

---
+++ src/test/java/pl/skyterix/springbootwildfly/SpringbootwildflyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skyterix.springbootwildfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwildflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
